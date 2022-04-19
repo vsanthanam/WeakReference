95766d48570369a3a59c7df36e345f497684f2a2
Varun Santhanam <talkto@vsanthanam.com>
2022-04-19 15:03:58 -0700

    Rename to WeakReference

--- generate-docs.sh
+++ generate-docs.sh
@@ -1,1 +1,1 @@
-swift package --allow-writing-to-directory docs/docc generate-documentation --target Weak --disable-indexing --transform-for-static-hosting --hosting-base-path docc --output-path docs/docc
+swift package --allow-writing-to-directory docs/docc generate-documentation --target WeakReference --disable-indexing --transform-for-static-hosting --hosting-base-path docc --output-path docs/docc
