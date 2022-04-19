f3d498b5b14efa995d1134933b408b0e6b1b7377
Varun Santhanam <talkto@vsanthanam.com>
2022-04-19 14:54:19 -0700

    Add Doc Generation Script

---
+++ generate-docs.sh
@@ -0,0 +1,1 @@
+swift package --allow-writing-to-directory docs/docc generate-documentation --target Weak --disable-indexing --transform-for-static-hosting --hosting-base-path docc --output-path docs/docc
